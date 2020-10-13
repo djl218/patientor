1eee4bc7c2e84b39d4257a179b1398dd0469bb67
Daniel Leskosky <dleskosky@gmail.com>
2020-10-13 12:14:08 -0400

    Commit for exercise 9.17

--- src/IndividualPatientPage/index.tsx
+++ src/IndividualPatientPage/index.tsx
@@ -1,47 +1,63 @@
 import React from "react";
-//import axios from "axios";
+import axios from "axios";
 import { Container } from "semantic-ui-react";
 
 import { Patient, MatchParams } from "../types";
-//import { apiBaseUrl } from "../constants";
+import { apiBaseUrl } from "../constants";
 import { useStateValue } from "../state";
 import { useRouteMatch } from 'react-router-dom';
-//import PatientListPage from "../PatientListPage";
 
 const IndividualPatientPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
-
   const match: MatchParams | null = useRouteMatch('/patients/:id');
-  console.log(match)
+  
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/api/patients/${match!.params.id}`
+        );
+        dispatch({ type: "SET_PATIENT", payload: patientFromApi });
+      } catch (e) {
+        console.error(e.response.data);
+      }
+    };
+    fetchPatient();
+  }, [match, dispatch]);
+
   const patient: Patient | null | undefined = match
     ? Object.values(patients).find((patient: Patient) => patient.id === match.params.id)
     : null;
 
-  /*const genderIcon = () => {
+  const genderIcon = () => {
     if (patient!.gender === "male") {
       return (
-        <Icon class="mars"></Icon>
+        <i className="mars icon"></i>
       )
     } else if(patient!.gender === "female") {
       return (
-        <Icon class="venus"></Icon>
+        <i className="venus icon"></i>
       )
     } else {
       return (
-        <Icon class="genderless"></Icon>
+        <i className="genderless icon"></i>
       )
     }
-  };*/
+  };
 
-  return (
-    <div className="App">
-      <Container textAlign="left">
-        <h3>{patient!.name}</h3>
-        <div><b>ssn: {patient!.ssn}</b></div>
-        <div><b>occupation: {patient!.occupation}</b></div>
-      </Container>
-    </div>
-  );
+  if (!patient) {
+    return null;
+  } else {
+    return (
+      <div className="App">
+        <Container textAlign="left">
+          <h3>{patient!.name}{genderIcon()}</h3>
+          <div><b>ssn: {patient!.ssn}</b></div>
+          <div><b>occupation: {patient!.occupation}</b></div>
+        </Container>
+      </div>
+    );
+  }
 };
 
 export default IndividualPatientPage;
