8326a2fe417891379eef623dd50946f6b4b99dfc
Daniel Leskosky <dleskosky@gmail.com>
2020-10-14 17:20:29 -0400

    Commit for exercise 9.20

--- src/IndividualPatientPage/index.tsx
+++ src/IndividualPatientPage/index.tsx
@@ -2,20 +2,32 @@ import React from "react";
 import axios from "axios";
 import { Container } from "semantic-ui-react";
 
-import { Patient, MatchParams } from "../types";
+import { Patient, MatchParams, Entry } from "../types";
 import { apiBaseUrl } from "../constants";
 import { useStateValue, setPatient } from "../state";
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, useParams } from 'react-router-dom';
+
+const EntryHelper: React.FC<{ code: string }> = ({code}) => {
+  return (
+    <li>{code}</li>
+  )
+};
+
+const NoEntryHelper: React.FC = () => {
+  return (
+    <div>no entries for patient</div>
+  )
+};
 
 const IndividualPatientPage: React.FC = () => {
   const [{ patients }, dispatch] = useStateValue();
-  const match: MatchParams | null = useRouteMatch('/patients/:id');
+  const { id } = useParams<{ id: string }>();
 
   React.useEffect(() => {
     const fetchPatient = async () => {
       try {
         const { data: patientFromApi } = await axios.get<Patient>(
-          `${apiBaseUrl}/api/patients/${match!.params.id}`
+          `${apiBaseUrl}/api/patients/${id}`
         );
         dispatch(setPatient(patientFromApi));
       } catch (e) {
@@ -23,8 +35,9 @@ const IndividualPatientPage: React.FC = () => {
       }
     };
     fetchPatient();
-  }, [match, dispatch]);
+  }, [dispatch, id]);
 
+  const match: MatchParams | null = useRouteMatch('/patients/:id');
   const patient: Patient | null | undefined = match
     ? Object.values(patients).find((patient: Patient) => patient.id === match.params.id)
     : null;
@@ -51,9 +64,31 @@ const IndividualPatientPage: React.FC = () => {
     return (
       <div className="App">
         <Container textAlign="left">
-          <h3>{patient!.name}{genderIcon()}</h3>
+          <h2>{patient!.name}{genderIcon()}</h2>
           <div><b>ssn: {patient!.ssn}</b></div>
           <div><b>occupation: {patient!.occupation}</b></div>
+          <h3>entries</h3>
+          {
+            patient.entries === undefined || patient.entries.length === 0 
+            ? <NoEntryHelper />
+            : patient.entries.map((entry: Entry) => (
+              <div key={entry.id}>
+              <div>{entry.date} <i>{entry.description}</i></div>
+              <ul>
+                {
+                  entry.diagnosisCodes === undefined
+                  ? null
+                  : entry.diagnosisCodes
+                    .map((code: string) =>
+                      <EntryHelper
+                        key={code}
+                        code={code}
+                      />  
+                    )    
+                }
+              </ul>
+              </div>
+          ))}
         </Container>
       </div>
     );
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+export interface MatchParams {
+  params: {id: string;};
+}
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -17,8 +21,51 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export interface MatchParams {
   params: {id: string;};
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthyCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthyCheckEntry;
